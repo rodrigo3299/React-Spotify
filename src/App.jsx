ab87a761e6edbfd6369e32f29646102ea9581f38
Caio Oliveira <caio.oliveira07@gmail.com>
2024-09-04 00:56:41 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+function App() {
+
+  return (
+    <>
+      <header className="bg-red-600 w-full h-28"></header>
+      <section className="w-full h-dvh flex">
+        <aside className="bg-green-400 w-1/4 flex flex-col justify-around items-center">
+          <div className="bg-red-500 w-32 h-32"></div>
+          <div className="bg-red-500 w-32 h-32"></div>
+          <div className="bg-red-500 w-32 h-32"></div>  
+        </aside>
+        <div className="bg-gray-400 w-3/4 grid grid-cols-4 pl-20 items-center">
+          <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-green-400 w-3/4 h-7"></div>
+            <div className="bg-green-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-green-400 w-3/4 h-7"></div>
+            <div className="bg-green-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-green-400 w-3/4 h-7"></div>
+            <div className="bg-green-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-green-400 w-3/4 h-7"></div>
+            <div className="bg-green-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-green-400 w-3/4 h-7"></div>
+            <div className="bg-green-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-green-400 w-3/4 h-7"></div>
+            <div className="bg-green-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-green-400 w-3/4 h-7"></div>
+            <div className="bg-green-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-green-400 w-3/4 h-7"></div>
+            <div className="bg-green-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-green-400 w-3/4 h-7"></div>
+            <div className="bg-green-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-green-400 w-3/4 h-7"></div>
+            <div className="bg-green-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-green-400 w-3/4 h-7"></div>
+            <div className="bg-green-400 w-3/4 h-7"></div>
+          </div>
+          <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
+            <div className="bg-green-400 w-3/4 h-7"></div>
+            <div className="bg-green-400 w-3/4 h-7"></div></div>  
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
